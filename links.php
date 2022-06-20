a8c4a60fc912ee937559d0670d87a383db5d8fa0
Jim Mortenson <nosnetrom@gmail.com>
2022-06-20 07:38:47 -0500

    Add Gumbo Kings to links page

--- links.php
+++ links.php
@@ -34,6 +34,9 @@
                 <p>Visit some of Steve's friends and cohorts at:</p>
                 <ul>
                     <li>
+                        N'Awlins Gumbo Kings: <a href="http://www.gumbokings.com">http://gumbokings.com</a>
+                    </li>
+                    <li>
                         John Tropea: <a style="color: var(--text); text-shadow: none;" target="_blank" href="https://www.johntropea.com">https://www.johntropea.com</a>
                     </li>
                     <li>
